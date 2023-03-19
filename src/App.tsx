36aa020c9b03b7add5b61e7218646934599a0fc2
jihyunglee <jhlee7125@gmail.com>
2023-03-19 13:42:36 +0900

    answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import JWTLogin from './pages/2-1/JWTLogin'
-import JWTLoginWithLocalStorage from './pages/2-2/JWTLoginWithLocalStorage'
-import AutoLogin from './pages/2-2/AutoLogin'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import JWTLogin from './pages/2-1/JWTLogin';
+import JWTLoginWithLocalStorage from './pages/2-2/JWTLoginWithLocalStorage';
+import AutoLogin from './pages/2-2/AutoLogin';
+import './App.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <JWTLogin/>,
+    path: '/',
+    element: <JWTLogin />,
   },
   {
-    path: "/local-storage",
-    element: <JWTLoginWithLocalStorage/>,
+    path: '/local-storage',
+    element: <JWTLoginWithLocalStorage />,
   },
   {
-    path: "/other-page",
-    element: <AutoLogin/>,
+    path: '/other-page',
+    element: <AutoLogin />,
   },
 ]);
 
 function App() {
   return (
-    <div className="content">
+    <div className='content'>
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
